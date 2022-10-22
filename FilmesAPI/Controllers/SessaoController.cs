556f8f42528fa89355bea664d6e2f35d3ecbcf0e
grbalmeida <g.r.almeida@live.com>
2022-10-22 11:25:08 -0300

    Cadastrando uma Sessão

---
+++ FilmesAPI/Controllers/SessaoController.cs
@@ -0,0 +1,48 @@
+﻿using AutoMapper;
+using FilmesAPI.Data;
+using FilmesAPI.Data.Dtos;
+using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SessaoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        private readonly IMapper _mapper;
+
+        public SessaoController(AppDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AdicionaSessao(CreateSessaoDto sessaoDto)
+        {
+            var sessao = _mapper.Map<Sessao>(sessaoDto);
+            _context.Sessoes.Add(sessao);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(RecuperaSessaoPorId), new { sessao.Id }, sessao);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> RecuperaSessaoPorId(int id)
+        {
+            var sessao = await _context.Sessoes.FirstOrDefaultAsync(sessao => sessao.Id == id);
+
+            if (sessao != null)
+            {
+                var sessaoDto = _mapper.Map<ReadSessaoDto>(sessao);
+                return Ok(sessaoDto);
+            }
+
+            return NotFound();
+        }
+    }
+}
---
+++ FilmesAPI/Data/Dtos/Sessao/ReadSessaoDto.cs
@@ -0,0 +1,13 @@
+﻿using FilmesAPI.Models;
+using System;
+
+namespace FilmesAPI.Data.Dtos
+{
+    public class ReadSessaoDto
+    {
+        public int Id { get; set; }
+        public Cinema Cinema { get; set; }
+        public Filme Filme { get; set; }
+        public DateTime HorarioDeEncerramento { get; set; }
+    }
+}
